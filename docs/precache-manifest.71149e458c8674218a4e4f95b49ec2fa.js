1d8e3bbdc4e6fd5750bdb2326e0ad1b897c7426f
Humblefool_01 <kaushikvishwanath010@gmail.com>
2020-04-22 15:30:45 +0530

    added docs folder for deployment v2

---
+++ docs/precache-manifest.71149e458c8674218a4e4f95b49ec2fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261c194d3bc34f933bcac5c03e670938",
+    "url": "/Football-Stats-App/docs/index.html"
+  },
+  {
+    "revision": "e22bf9dccd00f6644385",
+    "url": "/Football-Stats-App/docs/static/css/main.b4d361db.chunk.css"
+  },
+  {
+    "revision": "ae57223128eac3c18fa0",
+    "url": "/Football-Stats-App/docs/static/js/2.2ae81ca6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Football-Stats-App/docs/static/js/2.2ae81ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22bf9dccd00f6644385",
+    "url": "/Football-Stats-App/docs/static/js/main.e2d38b85.chunk.js"
+  },
+  {
+    "revision": "8ea30038a44b534f7984",
+    "url": "/Football-Stats-App/docs/static/js/runtime-main.60ee2984.js"
+  }
+]);
