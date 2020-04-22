9a70037805b1b005ce1f6ec8e894aa52ac1922eb
Humblefool_01 <kaushikvishwanath010@gmail.com>
2020-04-22 15:07:29 +0530

    Revert "Updates"
    This reverts commit ae306baa95f5a2ddc97209be6a2bd9512d1929c0.

---
+++ src/Loading.js
@@ -0,0 +1,130 @@
+import React from 'react'; 
+import './App.css';
+import PointsTable from './PointsTable';
+
+class Loading extends React.Component {        
+
+    constructor(props) {        
+        super(props);
+        this.data = null;
+        this.buttonID = -1;           
+        this.url = "";
+        this.bundesliga = 'http://api.football-data.org/v2/competitions/2002/standings';
+        this.ucl = 'http://api.football-data.org/v2/competitions/2001/standings';
+        this.epl = 'http://api.football-data.org/v2/competitions/2021/standings';  
+        this.eredivise = 'http://api.football-data.org/v2/competitions/2003/standings';
+        this.laliga = 'http://api.football-data.org/v2/competitions/2014/standings';
+        this.ligue1 = 'http://api.football-data.org/v2/competitions/2015/standings';
+        this.seriea = 'http://api.football-data.org/v2/competitions/2034/standings';
+        this.name = "";
+        this.state = {
+            received: false,
+        }
+        /*
+        2000: WC
+        2001: UCL
+        2002: Bundesliga
+        2021: EPL
+        2003: Eredivise
+        2014: LaLiga
+        2015: Ligue 1
+        2034: SerieA
+        */
+    }
+    
+    async componentDidMount() {
+        switch (this.buttonID) {
+            case 0:
+                this.url = this.laliga;
+                this.name="La Liga";
+                break;
+            case 1:
+                    this.url = this.ucl;
+                    this.name = "UEFA Champions League";
+                break;
+            case 2:
+                    this.url = this.bundesliga;
+                    this.name = "Bundesliga";
+                break;
+            case 3:
+                    this.url = this.epl;
+                    this.name = "Premier League";
+                break;
+            case 4:
+                    this.url = this.ligue1;
+                    this.name = "Ligue 1";
+                break;
+            case 5:
+                    this.url = this.eredivise;
+                    this.name = "Eredivise";
+                break;
+            default:
+                console.log("Switch case default error!");
+                break;
+        }
+
+        this.data = await this.fetchData();
+        if (this.data != null) {
+            this.setState({
+                received: true,
+            })
+        }
+    }
+
+
+    async fetchData() {
+        const rawData = await fetch(this.url, {
+            method: 'GET',
+            headers: {
+                'X-AUTH-TOKEN': '2df120a38d634333bc4c7a5c2a0c4cba',
+            }
+        })
+        const dataJSON = await rawData.json();
+        
+        return dataJSON;
+    }
+
+    render() { 
+        var backgroungImg = [];
+        this.buttonID = this.props.location.data;
+        if (this.data == null) {
+            return (
+                <div className="loader">                
+                </div>
+            );
+        }
+        else {
+            switch (this.buttonID) {
+                case 0:
+                        backgroungImg.push(<img className="BackgroundImgLaliga"></img>);
+                    break;
+                case 1:
+                        backgroungImg.push(<img className="BackgroundImgUCL"></img>);
+                    break; 
+                case 2:
+                        backgroungImg.push(<img className="BackgroundImgBundesliga"></img>);
+                    break;
+                case 3:
+                        backgroungImg.push(<img className="BackgroundImgEPL"></img>);
+                    break;
+                case 4:
+                        backgroungImg.push(<img className="BackgroundImgLigue1"></img>);
+                    break;
+                case 5:
+                        backgroungImg.push(<img className="BackgroundImgEredivise"></img>);
+                    break;
+            }
+            return (
+                <div>
+                    <nav><h1>{this.name}</h1></nav>
+                    <div className="PointsTablePage">
+                    <PointsTable id={this.buttonID} data={this.data} />                    
+                    </div>
+                </div>
+            );            
+        }                                             
+
+    }
+}
+
+export default Loading;
---
+++ src/PointsTable.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { concat } from 'rxjs';
+
+class PointsTable extends React.Component{
+    constructor(props) {
+        super(props);        
+        this.state = {
+            teamClicked: false,
+        }
+    }
+    render() {
+        const id = this.props.id;
+        const tableData = this.props.data;
+        const leagueTableData = tableData.standings[0].table;
+        var uclQ;
+        var table = [];
+        var groupName = "";
+
+        switch (id) {
+            case 1:
+                uclQ = 2;                
+                break;
+            case 4:
+                uclQ = 2;
+                break;
+            case 5:      
+                uclQ = 0;          
+                break;
+            default:
+                uclQ = 4;
+                break;
+        }
+
+        if (id != 1) {
+            table.push(this.TableHeader());
+            for (let i=0; i<leagueTableData.length; i++) {
+                const row = [];  
+                if (i+1 <= uclQ) {
+                    row.push(<td><div className="TdUcl">{i+1}</div></td>);    
+                }
+                else {
+                    row.push(<td>{i+1}</td>);    
+                }
+                row.push(<td><Link className="LinkTag" to={{pathname: "/TeamDetails", data: leagueTableData[i].team.id}}>
+                    <button key={i+1} className="TeamClass" onClick={() => {this.TeamButtonClicked(leagueTableData, i)}}>                            
+                            <img className="ImgLogo" src={leagueTableData[i].team.crestUrl} alt=""></img>
+                            <span style={{fontSize: "120%"}}>{leagueTableData[i].team.name}</span>
+                        </button>
+                        </Link>
+                    </td>);
+                row.push(<td>{leagueTableData[i].playedGames}</td>);
+                row.push(<td>{leagueTableData[i].won}</td>);
+                row.push(<td>{leagueTableData[i].draw}</td>);
+                row.push(<td>{leagueTableData[i].lost}</td>);
+                row.push(<td>{leagueTableData[i].points}</td>);
+                row.push(<td>{leagueTableData[i].goalsFor}</td>);
+                row.push(<td>{leagueTableData[i].goalsAgainst}</td>);
+                row.push(<td>{leagueTableData[i].goalDifference}</td>);
+
+                table.push(<tr>{row}</tr>)
+            }         
+            return(
+                <div>
+                    <table className="TableCard">  
+                        <tbody>     
+                            {table}
+                        </tbody>
+                    </table>    
+                </div>        
+            )
+        } else {
+            const outerCard = [];
+            for (let i=0; i< tableData.standings.length; i+=3) {
+                this.groupName = tableData.standings[i].group;
+                table = [];
+                table.push(this.TableHeader());
+                for (let j=0; j<tableData.standings[i].table.length; j++) {
+                    const groupTable = tableData.standings[i].table;
+                    const row = [];
+                    if (j+1 <= uclQ) {
+                        row.push(<td><div className="TdUcl">{j+1}</div></td>);
+                    }else {
+                        row.push(<td>{j+1}</td>);
+                    }
+                    row.push(<td>
+                        <Link className="LinkTag" to={{pathname: "/TeamDetails", data: groupTable[j].team.id}}>
+                        <button className="TeamClass" onClick={() => {this.TeamButtonClicked(groupTable, j)}}><img className="ImgLogo" src={groupTable[j].team.crestUrl} alt=""></img>
+                        <span style={{fontSize: "120%"}}>{groupTable[j].team.name}</span>
+                        </button>
+                        </Link>
+                        </td>);
+                    row.push(<td>{groupTable[j].playedGames}</td>);
+                    row.push(<td>{groupTable[j].won}</td>);
+                    row.push(<td>{groupTable[j].draw}</td>);
+                    row.push(<td>{groupTable[j].lost}</td>);
+                    row.push(<td>{groupTable[j].points}</td>);
+                    row.push(<td>{groupTable[j].goalsFor}</td>);
+                    row.push(<td>{groupTable[j].goalsAgainst}</td>);
+                    row.push(<td>{groupTable[j].goalDifference}</td>);
+
+                    table.push(<tr>{row}</tr>);
+                }
+                outerCard.push(<table className="TableCard">  
+                            <div className="GroupName"><b>{this.groupName}</b></div>
+                            <tbody>     
+                                {table}
+                            </tbody>
+                        </table>);
+            }
+            return (<div>{outerCard}</div>    
+            );
+        }
+    }
+
+    TeamButtonClicked(leagueTableData, teamIndex) {
+        console.log(leagueTableData[teamIndex].team.id, leagueTableData[teamIndex].team.name);
+        this.setState({
+            teamClicked: true,
+        })
+    }
+
+    TableHeader() {
+        const T = [];
+        const header = [];
+        header.push(<th>Pos</th>);
+        header.push(<th className="Th_Team_name">Team</th>);
+        header.push(<th>Matches</th>);
+        header.push(<th>Won</th>);
+        header.push(<th>Draw</th>);
+        header.push(<th>Lost</th>);
+        header.push(<th>Pts</th>);
+        header.push(<th>GF</th>);
+        header.push(<th>GA</th>);
+        header.push(<th>GD</th>);
+        T.push(<tr>{header}</tr>);
+        return T;
+    }
+    
+}
+
+export default PointsTable;
